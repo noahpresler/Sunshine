3a9028e37131f0859551c53ad7d56b2fa0dd53a4
Noah Presler <noah@presler.me>
2014-12-21 20:09:37 -0500

    complete list based UI using adapter
    used the adapter and test data. Not real.

--- app/src/main/java/me/presler/sunshine/sunshine/MainActivity.java
+++ app/src/main/java/me/presler/sunshine/sunshine/MainActivity.java
@@ -10,6 +10,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.*;
 
 
 public class MainActivity extends Activity {
@@ -59,7 +62,27 @@ public class MainActivity extends Activity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
+            String[] forecastArray = {
+                    "Today - Sunny - 88/63",
+                    "Tomorrow - Foggy - 70/40",
+                    "Weds - Cloudy - 72/63",
+                    "Thurs - Asteroids - 75/65",
+                    "Fri - Heavy Rain - 65/56",
+                    "Sat - asdfjadslkjflsajdfafdasdfasdf - 60/51",
+                    "Sun - sunny - 80/68"
+            };
+            List<String> weekForecast = new ArrayList<String>
+                    (Arrays.asList(forecastArray));
+            ArrayAdapter<String> mForecastAdapter =
+                    new ArrayAdapter<String>(
+                    getActivity(),
+                    R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textview,
+                    weekForecast
+            );
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(mForecastAdapter);
             return rootView;
         }
     }
