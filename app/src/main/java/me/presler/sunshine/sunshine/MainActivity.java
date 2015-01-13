8f451dd522d0101795889c475f8eba349ef579d2
Noah Presler <noah@presler.me>
2015-01-13 09:33:18 -0500

    creating settingsmenu

--- app/src/main/java/me/presler/sunshine/sunshine/MainActivity.java
+++ app/src/main/java/me/presler/sunshine/sunshine/MainActivity.java
@@ -64,6 +64,7 @@ public class MainActivity extends Activity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
