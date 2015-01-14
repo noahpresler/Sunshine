68ce8a275518b9dd65f63cd3375005e4e79b5fd7
Noah Presler <noah@presler.me>
2015-01-14 08:57:04 -0500

    adding weather entry/location entry to contract

--- app/src/main/java/me/presler/sunshine/sunshine/data/WeatherContract.java
+++ app/src/main/java/me/presler/sunshine/sunshine/data/WeatherContract.java
@@ -7,6 +7,26 @@ import android.provider.BaseColumns;
  * Defines table and column names for the weather database.
  */
 public class WeatherContract {
+
+    public static final class LocationEntry implements BaseColumns {
+
+        // Table name
+        public static final String TABLE_NAME = "location";
+
+        // The location setting string is what will be sent to openweathermap
+        // as the location query.
+        public static final String COLUMN_LOCATION_SETTING = "location_setting";
+
+        // Human readable location string, provided by the API.  Because for styling,
+        // "Mountain View" is more recognizable than 94043.
+        public static final String COLUMN_CITY_NAME = "city_name";
+
+        // In order to uniquely pinpoint the location on the map when we launch the
+        // map intent, we store the latitude and longitude as returned by openweathermap.
+        public static final String COLUMN_COORD_LAT = "coord_lat";
+        public static final String COLUMN_COORD_LONG = "coord_long";
+    }
+
     /* Inner class that defines the table contents of the weather table */
     public static final class WeatherEntry implements BaseColumns {
 
